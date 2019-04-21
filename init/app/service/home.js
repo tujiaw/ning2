6db2d6ca3f485c621580d8d6f84b54d99a45abf8
tujiaw <tujiaw@163.com>
2019-04-21 19:13:34 +0800

    add

--- init/app/service/home.js
+++ init/app/service/home.js
@@ -1,6 +1,84 @@
 const Service = require('egg').Service;
 const PostsModel = require('../model/posts');
 const MongoHelp = require('../model/mongo').mongoHelp;
+const moment = require('moment');
+const objectIdToTimestamp = require('objectid-to-timestamp');
+
+function getRandomItems(arr, num) {
+    //新建一个数组,将传入的数组复制过来,用于运算,而不要直接操作传入的数组;
+    var temp_array = new Array();
+    for (var index in arr) {
+        temp_array.push(arr[index]);
+    }
+    //取出的数值项,保存在此数组
+    var return_array = new Array();
+    for (var i = 0; i<num; i++) {
+        //判断如果数组还有可以取出的元素,以防下标越界
+        if (temp_array.length>0) {
+            //在数组中产生一个随机索引
+            var arrIndex = Math.floor(Math.random()*temp_array.length);
+            //将此随机索引的对应的数组元素值复制出来
+            return_array[i] = temp_array[arrIndex];
+            //然后删掉此索引的数组元素,这时候temp_array变为新的数组
+            temp_array.splice(arrIndex, 1);
+        } else {
+            //数组中数据项取完后,退出循环,比如数组本来只有10项,但要求取出20项.
+            break;
+        }
+    }
+    return return_array;
+}
+
+async function getRightSidebarData() {
+    let allPosts = await PostsModel.getPostsProfile();
+    allPosts = allPosts.sort((a, b) => ( b.pv - a.pv ));
+    const totalCount = allPosts.length;
+    const tagsCount = []
+    const archivesCount = {}
+    allPosts.forEach((post) => {
+      // 归档
+      const createYearMonth = moment(objectIdToTimestamp(post._id)).format('YYYY-MM-DD').substr(0, 7);
+      if (createYearMonth.length) {
+        if (archivesCount[createYearMonth]) {
+          archivesCount[createYearMonth]++;
+        } else {
+          archivesCount[createYearMonth] = 1;
+        }
+      }
+      
+      // 标签
+      post.tags.forEach((tag) => {
+          if (tag.length) {
+            const fitem = tagsCount.find((item) => ( item.name === tag ));
+            if (fitem) {
+              fitem.count++;
+            } else {
+              tagsCount.push({ name: tag, count: 1})
+            }
+          }
+      })
+    })
+    
+    const result = {}
+    result.profile = {
+      postCount: totalCount,
+      hitCount: 526,
+      hitToday: 235462
+    };
+    // 热搜
+    result.hotPosts = getRandomItems(allPosts.slice(0, 50), 10);
+    result.tagsCount = tagsCount;
+    result.archives = []
+    for (let item in archivesCount) {
+      result.archives.push({
+        yearMonth: item,
+        count: archivesCount[item]
+      })
+    }
+    result.archives.sort((a, b) => a.yearMonth > b.yearMonth ? -1 : 1);
+    console.log(result.hotPosts.length)
+    return result
+  }
 
 class HomeService extends Service {
     async list(page = 1) {
@@ -42,6 +120,7 @@ class HomeService extends Service {
             pageNumbers: pageNumbers,
             prevPage: prevPage,
             nextPage: nextPage,
+            right: await getRightSidebarData()
         }
         return result;
     }
---
+++ init/config/config.model.js
@@ -0,0 +1,29 @@
+module.exports = {
+    title: '泞途 - 泊客网',
+    desc: 'Keep It Simple, Stupid',
+    port: 3000,
+    pageCount: 20,
+    searchKeyCount: 8,
+    session: {
+      secret: 'nodeblog',
+      key: 'nodeblog',
+      maxAge: 2592000000,
+    },
+    tags: ['Windows', 'Linux', 'Android', 'IOS', 'Mac', 'Mobile', 'C/C++', 'Qt', 'Node.js', 'Java', 'Database', 
+          'Web', 'Tools', 'Bug', 'Life', 'Tips', 'Design', 'Javascript', 'MongoDB', 
+          'React', 'Product'],
+    //mongodb: 'mongodb\://localhost:27017/nodeblog', // :一定要转义
+    blogUri: 'mongodb\://127.0.0.1:27017/nodeblog',
+    poetryUri: 'mongodb\://127.0.0.1:27017/chinese-poetry',
+    qiniu: {
+      bucket: 'myimages',
+      ak: 'C1NMwcgz9IAjZVnpYF5LvuCJ6HV5MyJek68QhPfz',
+      sk: 'PcWuJ18qByV-hjo-EzSiND3WtdLB94-wRQUwMwCQ',
+    },
+    PREFIX_API: '/api',
+    github: {
+      client_id: '531ad8e4517595748d97',
+      client_secret: 'bf123fc9fe25a30e3e33d7a07daf825b73e07dc6',
+    },
+    bing: 'https://bing.biturl.top'
+  };
