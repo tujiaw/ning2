b79d43a9a70d5e79ea9501e51f1a9d03c54668da
tujiaw <tujiaw@163.com>
2019-05-05 17:47:00 +0800

    add

--- init/app/public/js/edit.js
+++ init/app/public/js/edit.js
@@ -1,14 +1,16 @@
-(function($) {
+'use strict';
+
+(function ($) {
   $('.edit select.dropdown').dropdown({
     action: 'activate',
-    onChange: function(value) {
+    onChange: function (value) {
       $('.edit #type').val(value);
     }
   });
 
   let tagList = $('.edit #selectedTag').val().split(',');
   tagList = tagList.filter(a => a);
-  $('.edit .itemTag').each(function() {
+  $('.edit .itemTag').each(function () {
     const name = $(this).children('label').html();
     if (tagList.indexOf(name) >= 0) {
       $(this).checkbox('check');
@@ -16,14 +18,14 @@
   });
 
   $('.edit .itemTag').checkbox({
-    onChecked: function() {
+    onChecked: function () {
       const name = $(this).siblings('label').html();
       if (tagList.indexOf(name) < 0) {
         tagList.push(name);
         $('.edit #selectedTag').val(tagList.join(','));
       }
     },
-    onUnchecked: function() {
+    onUnchecked: function () {
       const name = $(this).siblings('label').html();
       const index = tagList.indexOf(name)
       if (index >= 0) {
--- init/app/public/js/post.js
+++ init/app/public/js/post.js
@@ -11,3 +11,127 @@ $('#toc-head').on('click', function() {
   }
   $('#toc ul').toggle();
 });
+
+(function comments(sha1) {
+  const $body = (window.opera) ? (document.compatMode === 'CSS1Compat' ? $('html') : $('body')) : $('html,body'); // 这行是 Opera 的补丁, 少了它 Opera 是直接用跳的而且画面闪烁
+  $(document).ready(function() {
+    const gotoid = getCookie('goto_id');
+    if (gotoid && gotoid.length) {
+      setCookie('goto_id', '');
+      $body.animate({
+        scrollTop: $('#' + gotoid).offset().top,
+      }, 300);
+    }
+  });
+
+  function getCookie(c_name) {
+    if (document.cookie.length > 0) {
+      let c_start = document.cookie.indexOf(c_name + '=');
+      if (c_start !== -1) {
+        c_start = c_start + c_name.length + 1;
+        let c_end = document.cookie.indexOf(';', c_start);
+        if (c_end === -1) c_end = document.cookie.length;
+        return unescape(document.cookie.substring(c_start, c_end));
+      }
+    }
+    return '';
+  }
+
+  function setCookie(c_name, value, expiredays) {
+    const exdate = new Date();
+    exdate.setDate(exdate.getDate() + expiredays);
+    document.cookie = c_name + '=' + escape(value) +
+      ((expiredays == null) ? '' : ';expires=' + exdate.toGMTString());
+  }
+
+  const refreshCaptcha = function() {
+    $.ajax({
+      type: 'GET',
+      url: '/captcha',
+      success: result => {
+        if (result.data) {
+          $('#svgCaptcha').html(result.data);
+          $('#svgCaptcha').attr('value', result.text);
+        }
+      },
+    });
+  };
+
+  refreshCaptcha();
+  $('#svgCaptcha').on('click', function() {
+    refreshCaptcha();
+  });
+
+  $('.comments .submit').on('click', function() {
+    let errMsg = '';
+    const content = $('.commentInput #content').val();
+    const name = $('.commentInput #name').val();
+    if (content.length < 3 || content.length > 2048) {
+      errMsg = '评论不合法.';
+    } else if (name.length < 1 || name.length > 64) {
+      errMsg = '姓名不合法.';
+    } else {
+      const text = $('#captcha').val().toLowerCase();
+      const inputCaptcha = sha1.update(text).hex();
+      const dstCaptcha = $('#svgCaptcha').attr('value');
+      if (inputCaptcha.length && inputCaptcha === dstCaptcha) {
+        setCookie('goto_id', 'post_comments', 1);
+        return true;
+      }
+      errMsg = '验证码不正确，请重新输入或者单击刷新验证码.';
+    }
+    $('.warning.message span').html(errMsg);
+    $('.warning.message').show();
+    return false;
+  });
+
+  // 解决eggjs csrf报错
+  const csrftoken = getCookie('csrfToken');
+  function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+  }
+  $.ajaxSetup({
+    // eslint-disable-next-line object-shorthand
+    beforeSend: function(xhr, settings) {
+      if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+        xhr.setRequestHeader('x-csrf-token', csrftoken);
+      }
+    },
+  });
+
+  $('.comments #delete').on('click', function() {
+    const commentId = $(this).parents('.comment').attr('id');
+    if (commentId && commentId.length) {
+      $.ajax({
+        type: 'POST',
+        url: '/comments/remove/',
+        data: {
+          commentId,
+        },
+        success: result => {
+          if (result === 'success') {
+            $('#' + commentId).hide();
+          } else {
+            console.error(result)
+          }
+        },
+      });
+    }
+  });
+
+  $('.comments #reply').on('click', function() {
+    // eslint-disable-next-line newline-per-chained-call
+    const author = $(this).parent().siblings('.author').text();
+    if (author.length) {
+      const inputContent = $('textarea#content');
+      inputContent.val('@' + author + ' ');
+      inputContent.focus();
+      $body.animate({
+        scrollTop: inputContent.offset().top,
+      }, 300);
+    }
+  });
+// eslint-disable-next-line no-undef
+})(sha1);
+
